e0b79c7fa56d57c394cc69aaecb9bea38c415c1e
meta4icalbot <2404353l@student.gla.ac.uk>
2020-01-29 13:52:04 +0000

    Chapter 4 completed (static+media)

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	html = "<html><body>Rango says hey there partner! <a href='/rango/about/'>About</a></body></html>"
+	#html = "<html><body>Rango says hey there partner! <a href='/rango/about/'>About</a></body></html>"
 	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	html = "<html><body>Rango says here is the about page. <a href='/rango/'>Index</a></body></html>"
-	return HttpResponse(html)
+	#html = "<html><body>Rango says here is the about page. <a href='/rango/'>Index</a></body></html>"
+	context_dict = {'boldmessage': 'This tutorial has been put together by meta4icalbot'}
+	return render(request, 'rango/about.html', context=context_dict)
