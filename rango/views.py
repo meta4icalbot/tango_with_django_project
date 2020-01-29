154f60c9c713892e82c38c4664b8caafe0e01d41
meta4icalbot <2404353l@student.gla.ac.uk>
2020-01-29 11:50:07 +0000

    Sections up to 4.2 completed-static media served successfully

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
 	html = "<html><body>Rango says hey there partner! <a href='/rango/about/'>About</a></body></html>"
-	return HttpResponse(html)
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
 	html = "<html><body>Rango says here is the about page. <a href='/rango/'>Index</a></body></html>"
