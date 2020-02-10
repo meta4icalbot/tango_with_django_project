0ea205f9bdfdf7857ae7d2600fc25a85e973e976
meta4icalbot <2404353l@student.gla.ac.uk>
2020-02-10 21:53:47 +0000

    Chapter 8 completed (+exercises) + errors from Ch. 7 corrected

--- rango/views.py
+++ rango/views.py
@@ -40,7 +40,7 @@ def add_category(request):
 		
 		if form.is_valid():
 			form.save(commit=True)
-			return redirect('/rango/')
+			return redirect(reverse('rango:index'))
 		else:
 			print(form.errors)
 	return render(request, 'rango/add_category.html', {'form': form})
@@ -52,7 +52,7 @@ def add_page(request, category_name_slug):
 		category = None
 	
 	if category == None:
-		return redirect('/rango/')
+		return redirect(reverse('rango:index'))
 	
 	form = PageForm()
 
